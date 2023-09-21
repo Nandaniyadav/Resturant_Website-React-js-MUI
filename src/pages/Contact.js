0f7712f78519dd99af72af1ccb0689c95e3759e8
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-09-21 14:41:46 +0530

    add contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,60 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
-import { Box, Typography } from '@mui/material'
+import { Box, Paper, Tab, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import EmailIcon from '@mui/icons-material/Email';
+import AddIcCallIcon from '@mui/icons-material/AddIcCall';
 
 const Contact = () => {
   return (
     <Layout>
-     <h1>Contact page</h1>
+      <Box sx={{
+        my: 10, ml: 10,
+        "& h4": { fontWeight: 'bold', mb: 2 }
+      }}>
+        <Typography variant='h4'>
+          Contact My Resturant
+        </Typography>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere labore repudiandae eveniet, quae qui omnis aliquid modi nam repellendus consequuntur rerum eos placeat corporis tempora! Vero ipsa dolores cumque blanditiis.</p>
+      </Box>
+      <Box sx={{m:3, width:'600px', ml:5, "@media (max-width:600px)":{
+        width:'300px'
+      }}}>
+        <TableContainer component={Paper}>
+          <Table aria-label="contact table">
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{bgcolor:'black', color:'white' }} align='center'>Contact Details</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <SupportAgentIcon sx={{ color: 'red', pt: 1 }} /> 1800-000-00  (Toll Free)
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell>
+                  <EmailIcon sx={{ color: 'skyblue', pt: 1 }} /> help@myrest.com
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <AddIcCallIcon sx={{ color: 'green', pt: 1 }} /> 1020304050
+                </TableCell>
+              </TableRow>
+
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
     </Layout>
   )
 }
 
 export default Contact
+
+
+
+
