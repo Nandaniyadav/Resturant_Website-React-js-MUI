6e538ae25faf6de5b43310a7137be25bbc5060af
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-09-21 10:45:53 +0530

    update code

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,30 +1,76 @@
-import React { useState } from "react";
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import React from "react";
+import {
+  AppBar,
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import FastfoodIcon from "@mui/icons-material/Fastfood";
-import MenuBookIcon from "@mui/icons-material/MenuBook";
+import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from "react-router-dom";
 import "../../styles/HeaderStyle.css";
-
+import { useState } from "react";
 
 const Header = () => {
-  const [mobileOpen, setmobileOpen] = useState(false)
+  const [mobileOpen, setMobileOpen] = useState(false);
 
-  
+  //for handle menu click
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  //Menu Drawer
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography
+        color={"goldenrod"}
+        variant="h6"
+        component="div"
+        sx={{ flexGrow: 1, my:2 }}
+      >
+        <FastfoodIcon />
+        My Resturant
+        <Divider/>
+      </Typography>
+      <ul className="mobile-navigation">
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/menu"}>Menu</Link>
+        </li>
+        <li>
+          <Link to={"/about"}>About</Link>
+        </li>
+        <li>
+          <Link to={"/contact"}>Contact</Link>
+        </li>
+      </ul>
+    </Box>
+  );
+
 
   return (
     <div>
-      <Box sx={{ display: { xs: "none", sm: "block" } }}>
+      <Box>
         <AppBar component={"nav"} sx={{ bgcolor: "black" }}>
           <Toolbar>
             <IconButton
               color="inherit"
-              area-aria-label="open drwaer"
+              area-aria-label="open drawer"
               edge="start"
-              sx={{ 
+              sx={{
                 mr: 2,
-              display:{ sm : "none"},}}
+                display:{ sm : "none"},
+              }}
+              
+              onClick={handleDrawerToggle}
             >
-              <MenuBookIcon />
+              <MenuIcon />
             </IconButton>
             <Typography
               color={"goldenrod"}
@@ -35,16 +81,16 @@ const Header = () => {
               <FastfoodIcon />
               My Resturant
             </Typography>
-            <Box>
+            <Box sx={{ display: { sx: "none", sm: "block" } }}>
               <ul className="navigation-menu">
                 <li>
-                  <Link to={"/"}>Home</Link>{" "}
+                  <Link to={"/"}>Home</Link>
                 </li>
                 <li>
-                  <Link to={"/menu"}>Menu</Link>{" "}
+                  <Link to={"/menu"}>Menu</Link>
                 </li>
                 <li>
-                  <Link to={"/about"}>About</Link>{" "}
+                  <Link to={"/about"}>About</Link>
                 </li>
                 <li>
                   <Link to={"/contact"}>Contact</Link>
@@ -53,6 +99,25 @@ const Header = () => {
             </Box>
           </Toolbar>
         </AppBar>
+        <Box component="nav">
+          <Drawer
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            sx={{ display: { xs: "block", sm: "none" }, "& .MuiDrawer-paper" :{
+              boxSizing:'border-box',
+              width:"240px",
+              
+            },
+          }}
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box>
+        <Toolbar />
+        </Box>
+        
       </Box>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
+import { Link } from 'react-router-dom'
+import Banner from '../images/banner.jpg'
+import "../styles/HomeStyle.css"
 
 const Home = () => {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <div className="home" style={{ backgroundImage: `url(${Banner})` }}>
+        <div className="headercontainer">
+          <h1>Food Website</h1>
+          <p>Best Food in India</p>
+          <Link>
+            <button>Order Now</button>
+          </Link>
+        </div>
+      </div>
     </Layout>
   )
 }
